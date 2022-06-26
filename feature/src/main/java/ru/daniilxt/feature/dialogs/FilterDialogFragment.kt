4be0a8e4cfa77d388ecc432cb430ccac296fa192
daniilxt <daniil-firsov@mail.ru>
2022-06-26 20:58:09 +0300

    Created filter dialog

---
+++ feature/src/main/java/ru/daniilxt/feature/dialogs/FilterDialogFragment.kt
@@ -0,0 +1,77 @@
+package ru.daniilxt.feature.dialogs
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.RadioGroup
+import androidx.core.view.children
+import androidx.core.view.get
+import androidx.fragment.app.DialogFragment
+import com.google.android.material.radiobutton.MaterialRadioButton
+import ru.daniilxt.common.extensions.setDebounceClickListener
+import ru.daniilxt.feature.R
+import ru.daniilxt.feature.databinding.FragmentFilterDialogBinding
+import ru.daniilxt.feature.domain.model.FilterType
+
+class FilterDialogFragment : DialogFragment() {
+    private var _binding: FragmentFilterDialogBinding? = null
+    private val binding get() = _binding!!
+    private var onOkClickListener: ((filterType: FilterType) -> Unit)? = null
+
+    private val dialogContext get() = binding.root.context
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFilterDialogBinding.inflate(inflater, container, false)
+
+        dialog?.window?.setDimAmount(DIM_ALPHA)
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        setupDialogView()
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.mbOk.setDebounceClickListener {
+            onOkClickListener?.invoke(getFilterType())
+            dialog?.dismiss()
+        }
+    }
+
+    fun setOnOkClickListener(onClick: (filterType: FilterType) -> Unit) {
+        onOkClickListener = onClick
+    }
+
+    private fun setupDialogView() {
+        (binding.radioGroup[0] as MaterialRadioButton).isChecked = true
+    }
+
+    fun getFilterType(): FilterType {
+        val rb =
+            binding.radioGroup.getChildAt(binding.radioGroup.checkedIndex) as MaterialRadioButton
+        return when (rb.text) {
+            dialogContext.getString(R.string.alphabetically_asc) -> FilterType.ALPHABETICALLY_ASC
+            dialogContext.getString(R.string.alphabetically_desc) -> FilterType.ALPHABETICALLY_DESC
+            dialogContext.getString(R.string.value_asc) -> FilterType.VALUE_ASC
+            dialogContext.getString(R.string.value_desc) -> FilterType.VALUE_DESC
+            else -> throw IllegalArgumentException("Incorrect state")
+        }
+    }
+
+    companion object {
+        val RadioGroup.checkedIndex: Int
+            get() = children
+                .indexOfFirst { it.id == checkedRadioButtonId }
+
+        private const val DIM_ALPHA = 0.25F
+        fun newInstance(): FilterDialogFragment {
+            return FilterDialogFragment()
+        }
+    }
+}
--- feature/src/main/java/ru/daniilxt/feature/main_screen/presentation/MainScreenFragment.kt
+++ feature/src/main/java/ru/daniilxt/feature/main_screen/presentation/MainScreenFragment.kt
@@ -4,18 +4,25 @@ import android.os.Bundle
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
+import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.google.android.material.tabs.TabLayoutMediator
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import ru.daniilxt.common.base.BaseFragment
 import ru.daniilxt.common.di.FeatureUtils
+import ru.daniilxt.common.extensions.setDebounceClickListener
 import ru.daniilxt.common.extensions.setLightStatusBar
 import ru.daniilxt.common.extensions.setStatusBarColor
+import ru.daniilxt.common.extensions.showDialog
 import ru.daniilxt.feature.R
 import ru.daniilxt.feature.databinding.FragmentMainScreenBinding
 import ru.daniilxt.feature.di.FeatureApi
 import ru.daniilxt.feature.di.FeatureComponent
+import ru.daniilxt.feature.dialogs.FilterDialogFragment
 import ru.daniilxt.feature.favorite.presentation.FavoriteFragment
 import ru.daniilxt.feature.interactors.IUpdatable
 import ru.daniilxt.feature.main_screen.presentation.adapter.MainScreenViewPagerAdapter
@@ -35,6 +42,13 @@ class MainScreenFragment : BaseFragment<MainScreenViewModel>(R.layout.fragment_m
             this, listOf(PopularFragment.newInstance(), FavoriteFragment.newInstance())
         )
     }
+    private val filterDialog by lazy {
+        FilterDialogFragment.newInstance().apply {
+            setOnOkClickListener {
+                Toast.makeText(requireContext(), "$it", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -45,6 +59,13 @@ class MainScreenFragment : BaseFragment<MainScreenViewModel>(R.layout.fragment_m
     override fun setupViews() {
         super.setupViews()
         initViewPager()
+        setupButtons()
+    }
+
+    private fun setupButtons() {
+        binding.mbFilter.setDebounceClickListener {
+            parentFragmentManager.showDialog(filterDialog)
+        }
     }
 
     override fun setupViewModelSubscriber() {
@@ -68,7 +89,10 @@ class MainScreenFragment : BaseFragment<MainScreenViewModel>(R.layout.fragment_m
     private fun setSpinnerListAdapter(spinner: AutoCompleteTextView, data: List<String>) {
         binding.spinnerCurrency.spinnerText.addTextChangedListener(beforeTextChanged = { text, _, _, _ ->
             // Call update rv in view pager child frg
-            (currentViewPagerFrg as? IUpdatable)?.update(text.toString())
+            lifecycleScope.launch {
+                delay(300L)
+                (currentViewPagerFrg as? IUpdatable)?.update(text.toString())
+            }
         })
         val adapter = ArrayAdapter(requireContext(), R.layout.spinner_text_item, data)
         spinner.setText(data.first())
