b56b649960d7d04352c977ce0567b7e4196ac3d7
daniilxt <daniil-firsov@mail.ru>
2022-06-27 19:22:30 +0300

    CurrencyViewHolder refactoring

--- feature/src/main/java/ru/daniilxt/feature/shared_adapter/view_holder/CurrencyViewHolder.kt
+++ feature/src/main/java/ru/daniilxt/feature/shared_adapter/view_holder/CurrencyViewHolder.kt
@@ -2,6 +2,7 @@ package ru.daniilxt.feature.shared_adapter.view_holder
 
 import ru.daniilxt.common.base.BaseViewHolder
 import ru.daniilxt.common.extensions.setDebounceClickListener
+import ru.daniilxt.feature.R
 import ru.daniilxt.feature.databinding.ItemCurrencyCardBinding
 import ru.daniilxt.feature.domain.model.Currency
 
@@ -10,8 +11,16 @@ class CurrencyViewHolder(private val binding: ItemCurrencyCardBinding) :
     fun bind(item: Currency, onFavoriteItemClickListener: (Currency) -> Unit) {
         binding.tvCurrency.text = item.name
         binding.chipCurrencyValue.text = item.value.toString()
-        binding.ibLike.isEnabled = item.isFavorite
+        binding.ibLike.setImageResource(
+            when (item.isFavorite) {
+                true -> R.drawable.ic_star_24
+                else -> R.drawable.ic_star_outline_24
+            }
+        )
         binding.ibLike.setDebounceClickListener {
+            onFavoriteItemClickListener(item)
+        }
+        itemView.setDebounceClickListener {
             onFavoriteItemClickListener(item)
         }
     }
